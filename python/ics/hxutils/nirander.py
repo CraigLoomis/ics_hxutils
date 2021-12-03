ca8316a60a640b3d9c5d8adbd6d785ecb9b87655
Craig Loomis <cloomis@astro.princeton.edu>
2021-12-03 16:17:53 -0500

    Too big a pile of cleanups and additions.

--- python/ics/hxutils/nirander.py
+++ python/ics/hxutils/nirander.py
@@ -373,14 +373,27 @@ class GimbalIlluminator(Illuminator):
         
         xNew, yNew = self.moveTo(xstep, ystep, preload=preload)
         return self.stepsToPix([xNew, yNew])
-        
+
+    def getTargetPosition(self, wave, row):
+        """Get the final x,y pixel position for a wave+row. Applies a .nudge if opne exists. """
+
+        led = self.leds[self.leds.wave == wave]
+        col = int(led.position)
+        pos = (row, col)
+        try:
+            pos = self.nudges[pos]
+        except KeyError:
+            pass
+
+        return pos
+
     def home(self, doX=True, doY=True):
         """Home one or more axes. Both by default. The controller leaves it """       
 
         if doX:
-            self._cmd("home x", maxTime=100)
+            self.dev.cmd("home x", maxTime=100)
         if doY:
-            self._cmd("home y", maxTime=100)
+            self.dev.cmd("home y", maxTime=100)
         return self.getSteps()
 
 def takeSuperDark(meade, nexp=3, nread=3, force=False, cam='n1'):
@@ -398,11 +411,17 @@ def takeSuperDark(meade, nexp=3, nread=3, force=False, cam='n1'):
     superDark = darkCube.DarkCube.createFromVisits(visits)
     return superDark
 
-def takeRamp(cam, nread, nreset=1, exptype="test", comment="", quiet=True):
-    pfsutils.oneCmd(f'hx_{cam}', f'ramp nread={nread} nreset={nreset} exptype={exptype} objname=\"{comment}\"', quiet=quiet)
-    visit = hx.pathToVisit(hx.lastRamp(cam=cam))
+def takeRamp(cam, nread, nreset=1, exptype="test", comment="no_comment", quiet=True):
+    pfsutils.oneCmd(f'hx_{cam}', f'ramp nread={nread} exptype={exptype} objname=\"{comment}\"',
+                    quiet=quiet)
+    visit = hxramp.pathToVisit(hxramp.lastRamp(cam=cam))
     
     return visit
+
+def moveFocus(cam, piston):
+    """Move the FPA focus."""
+
+    pfsutils.oneCmd(f'xcu_{cam}', f'motors move piston={piston} abs microns')
 
 def motorScan(meade, xpos, ypos, led=None, call=None, nread=3, posInPixels=True):
     """Move to the given positions and acquire spots.
@@ -542,7 +561,7 @@ def createDither(frames, hxCalib, rad=15, doNorm=False):
     xsteps = frames['xstep'].unique()
     ysteps = frames['ystep'].unique()
     
-    xoffsets = {xs:xi for xi,xs in enumerate(xsteps)}
+    xoffsets = {xs:(scale-1)-xi for xi,xs in enumerate(xsteps)}
     yoffsets = {ys:(scale-1)-yi for yi,ys in enumerate(ysteps)}
     # Need better sanity checks
     if len(frames) != scale*scale or len(xsteps) != scale or len(ysteps) != scale:
@@ -564,7 +583,7 @@ def createDither(frames, hxCalib, rad=15, doNorm=False):
         im = hxCalib.isr(int(f1.visit))
         im = im[yslice,xslice].astype('f4')
         maskedIm = im*bkgndMask
-        bkgnd = np.median(maskedIm[np.where(maskedIm>0)])
+        bkgnd = np.median(maskedIm[np.where(maskedIm > 0)])
         im -= bkgnd
         maskedIm = im*bkgndMask
 
@@ -584,8 +603,34 @@ def createDither(frames, hxCalib, rad=15, doNorm=False):
 
     return outIm, outIms
 
+def ditherPath(butler, row, pfsDay=None):
+    if pfsDay is None:
+        path = butler.get('dither',
+                          idDict=dict(visit=int(row.visit),
+                                      wave=int(row.wavelength),
+                                      focus=row.focus,
+                                      row=(np.round(row.ypix/100)*100)))
+    else:
+        path = butler.search('dither',
+                             idDict=dict(visit=int(row.visit),
+                                         wave=int(row.wavelength),
+                                         focus=row.focus,
+                                         row=(np.round(row.ypix/100)*100)),
+                             pfsDay=pfsDay)
+
+    return path
+
+def ditherPaths(butler, rows, pfsDay='*'):
+    paths = []
+    for i in range(0, len(rows), 9):
+        row = rows.iloc[i]
+        path = ditherPath(butler, row, pfsDay=pfsDay)[0]
+        paths.append(path)
+    return paths
+
 def allDithers(frames, hxCalib, rad=15, butler=None, doNorm=False):
     dithers = []
+    ids = []
     for i in range(len(frames)//9):
         dithFrames = frames.iloc[i*9:(i+1)*9]
         print(len(dithFrames))
@@ -594,11 +639,12 @@ def allDithers(frames, hxCalib, rad=15, butler=None, doNorm=False):
 
         if butler is not None:
             row = dithFrames.iloc[0]
-            path = butler.get('dither', 
-                              idDict=dict(visit=int(row.visit),
-                                          wave=int(row.wavelength),
-                                          focus=row.focus,
-                                          row=(np.round(row.ypix/100)*100)))
+            idDict = dict(visit=int(row.visit),
+                          wave=int(row.wavelength),
+                          focus=row.focus,
+                          row=(np.round(row.ypix/100)*100))
+            ids.append(idDict)
+            path = butler.get('dither', idDict=idDict)
             hdr = [dict(name='VISIT', value=int(row.visit), comment="visit of 0,0 image"),
                    dict(name='WAVE', value=row.wavelength),                   
                    dict(name='FOCUS', value=row.focus),
@@ -611,6 +657,8 @@ def allDithers(frames, hxCalib, rad=15, butler=None, doNorm=False):
                    dict(name='PEAK', value=row.peak, comment="measured peak of 0,0 image")]
             path.parent.mkdir(parents=True, exist_ok=True)
             fitsio.write(path, dith1, header=hdr, clobber=True)
+            logger.info(f'wrote dither {path}')
+    return ids
 
 def ditherAt(meade, led, row, nramps=3, npos=3, nread=3, xsteps=5, ysteps=2):
     """Acquire dithered imaged at a given position. """
@@ -632,7 +680,7 @@ def ditherAt(meade, led, row, nramps=3, npos=3, nread=3, xsteps=5, ysteps=2):
     return pd.concat(visits, ignore_index=True)
 
 def ditherSet(meade, butler=None, waves=None, rows=[88,2040,3995], focus=122.0,
-              nramps=3, takeDarks=True):
+              nramps=1, takeDarks=False):
     if waves is None:
         waves = meade.leds.wave
     if np.isscalar(waves):
@@ -665,7 +713,7 @@ def ditherSet(meade, butler=None, waves=None, rows=[88,2040,3995], focus=122.0,
                     try:
                         ret = ditherAt(meade, w, row, nramps=nramps)
                     except Exception as e:
-                        breakpoint()
+                        raise
 
                     ret['focus'] = f
                     ret['wavelength'] = w
@@ -675,14 +723,14 @@ def ditherSet(meade, butler=None, waves=None, rows=[88,2040,3995], focus=122.0,
                     print("ditherList: ", len(ditherList))
                     rowFrame =  pd.concat(ditherList, ignore_index=True)
                     if butler is not None:
-                        outFileName = butler.get('measures', idDict=dict(visit=rowFrame.visit.min()))
+                        outFileName = butler.getPath('measures', idDict=dict(visit=rowFrame.visit.min()))
                         outFileName.parent.mkdir(mode=0o2775, parents=True, exist_ok=True)
                         with open(outFileName, mode='w') as outf:
                             outf.write(rowFrame.to_string())
                             print(f"wrote {len(rowFrame)} lines to {outFileName} at led {w} on row {row} with focus {f}")
     except Exception as e:
         print(f'oops: {e}')
-        breakpoint()
+        # breakpoint()
         raise
     finally:
         meade.ledsOff()
@@ -694,21 +742,28 @@ def trimRect(im, c, r=100):
     
     return im2.copy()
     
-def getPeaks(im, thresh=250.0, mask=None, center=None, radius=100):
+def getPeaks(im, thresh=250.0, mask=None, center=None, radius=100,
+             convolveSigma=None, kernel=None):
     bkgnd = sep.Background(im, mask=mask)
     bkg = np.array(bkgnd)
     corrImg = im - bkg
+
+    if convolveSigma is not None:
+        corrImg = scipy.ndimage.gaussian_filter(corrImg, sigma=convolveSigma)
     
     # Doing this the expensive way: extract on full image, then trim
     spots = sep.extract(corrImg, deblend_cont=1.0, 
-                        thresh=thresh, mask=mask)
+                        thresh=thresh, mask=mask,
+                        filter_kernel=kernel)
     spotsFrame = pd.DataFrame(spots)
+    spotsFrame['ellipticity'] = spotsFrame.x2 / spotsFrame.y2
+    spotsFrame.loc[spotsFrame.ellipticity < 1,
+                   'ellipticity'] = 1/spotsFrame.loc[spotsFrame.ellipticity < 1,
+                                                     'ellipticity']
     if center is not None:
+        center = np.atleast_2d(center)
         keep_w = cdist(spotsFrame[["x","y"]], center) <= radius
         spotsFrame = spotsFrame.loc[keep_w]
-        if len(spotsFrame) != 1:
-            print(f'got {len(spotsFrame)} spots near {center}')
-            spotsFrame = None
 
     return corrImg, spotsFrame
 
@@ -747,9 +802,9 @@ def showPeaks2(corrImg, spots=None, nsig=3.0, imrad=100):
 
             # plot an ellipse for each object
             e = Ellipse(xy=(s['x'], s['y']),
-                width=6*s['a'],
-                height=6*s['b'],
-                angle=s['theta'] * 180. / np.pi)
+                        width=6*s['a'],
+                        height=6*s['b'],
+                        angle=s['theta'] * 180. / np.pi)
             e.set_facecolor('none')
             e.set_edgecolor('red')
             p1.add_artist(e)
@@ -804,7 +859,7 @@ def _scanForFocus(center, spacing, r, nread=3, cam='n1', measureCall=None):
     visits = []
     for f in focusReq:
         pfsutils.oneCmd(f'xcu_{cam}', f"motors move piston={f} abs microns")
-        visit = takeRamp(cam=cam, nread=nread)
+        visit = takeRamp(cam=cam, nread=nread, comment=f'focus_sweep:{f}')
         visits.append(visit)
         
     scanFrame = pd.DataFrame(dict(visit=visits, focus=focusReq))
@@ -819,7 +874,7 @@ def _scanForFocus(center, spacing, r, nread=3, cam='n1', measureCall=None):
                 bestFocus <= focusReq[-1]):
 
                 pfsutils.oneCmd(f'xcu_{cam}', f"motors move piston={bestFocus} abs microns")
-                visit = takeRamp(cam=cam, nread=nread)
+                visit = takeRamp(cam=cam, nread=nread, comment=f'at_best_focus:{bestFocus}')
                 visits.append(visit)
 
                 bestSize = focusPoly(bestFocus)
@@ -878,7 +933,14 @@ def scanForFocus(center, spacing=5, r=4, measureCall=None):
 def scanForCrudeFocus(center, spacing=25, r=3, measureCall=None):
     return _scanForFocus(center, spacing=spacing, r=r, measureCall=measureCall)
 
-def measureSet(scans, hxCalib, thresh=250, center=None, radius=100, skipDone=False):
+def stepsToPix(xstep, ystep):
+    # This is absolutely disguting.
+    meade =  GimbalIlluminator()
+    return meade.stepsToPix([xstep, ystep])
+
+def measureSet(scans, hxCalib=None, thresh=1000, center=None,
+               radius=100, skipDone=True, ims=None, trimBad=True,
+               convolveSigma=None, kernel=None):
     """Measure the best spots in a DataFrame of images
     
     Parameters
@@ -886,22 +948,22 @@ def measureSet(scans, hxCalib, thresh=250, center=None, radius=100, skipDone=Fal
     scans : `pd.DataFrame`
         [description]
     hxCalib : `HxCalib`
-        [description]
+        An object which can .isr() an image.
     thresh : int, optional
-        [description], by default 250
-    center : [type], optional
-        [description], by default None
+        The detection threshold in ADU, by default 250
+    center : (float, float), optional
+        The expected pixel center, by default None
     radius : int, optional
-        [description], by default 100
+        How far to look for a spot, by default 100
     skipDone : bool, optional
-        [description], by default True
+        Do not reprocess already measured rows, by default True
     
     Returns
     -------
     [type]
         [description]
     """
-       
+
     if hxCalib is None:
         hxCalib = hxramp.HxCalib()
 
@@ -914,22 +976,50 @@ def measureSet(scans, hxCalib, thresh=250, center=None, radius=100, skipDone=Fal
     else:
         notDone = scans.index
 
-    for scan_i in notDone:        
-        if center is None:
-            pixCenter = [scans.loc[scan_i, 'xstep'], scans.loc[scan_i, 'ystep']]
-            center_i = stepToPix(pixCenter)
-            # print(f"{scan_i} center={center} from {pixCenter}")
+    for scan_i in notDone:
+        if center is False:
+            center_i = None
+        elif center is None:
+            # Try for a measured position. Failing that, use the steps.
+            center_i = (scans.loc[scan_i, 'xpix'], scans.loc[scan_i, 'ypix'])
+            if np.isnan(center_i[0]) or np.isnan(center_i[1]):
+                try:
+                    stepCenter = (scans.loc[scan_i, 'xstep'], scans.loc[scan_i, 'ystep'])
+                    center_i = stepsToPix(*stepCenter)
+                    logger.info((f"{scan_i} center from steps: {center_i}"))
+                except Exception as e:
+                    logger.warn(f'failed to get a center for {scans.loc[scan_i]}: {e}')
+                    center_i = None
+            else:
+                logger.info((f"{scan_i} center from pix: {center_i}"))
+
         else:
             center_i = center
-            
-        corrImg, spots = getPeaks(hxCalib.isr(scans.loc[scan_i, 'visit']),
+
+        if ims is not None:
+            corrImg = ims[scan_i]
+            center_i = None
+        else:
+            if hxCalib is not None:
+                corrImg = hxCalib.isr(scans.loc[scan_i, 'visit'])
+            else:
+                ramp = hxramp.HxRamp(visit=scans.loc[scan_i, 'visit'])
+                corrImg = ramp.cds()
+
+        corrImg, spots = getPeaks(corrImg,
                                   center=center_i, radius=radius,
                                   thresh=thresh, 
-                                  mask=hxCalib.badMask)
-        if spots is None:
+                                  mask=hxCalib.badMask,
+                                  convolveSigma=convolveSigma,
+                                  kernel=kernel)
+        if trimBad and len(spots) > 0:
+            ok = (spots.size < 150) & (spots.ellipticity < 5.0)
+            origSpots = spots.copy()
+            spots = spots[ok]
+        if spots is None or len(spots) == 0:
             print(f"nope: i={scan_i}, scan={scans.loc[scan_i]}")
-        if spots is not None:
-            print(f"    : i={scan_i}, visit={scans.loc[scan_i, 'visit']}")
+        else:
+            print(f"    : i={scan_i}, visit={scans.loc[scan_i, 'visit']}, nspots={len(spots)}")
             bestSpot = spots.loc[spots.flux.idxmax()]
             scans.loc[scan_i, 'xpix'] = bestSpot.x
             scans.loc[scan_i, 'ypix'] = bestSpot.y
